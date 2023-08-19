f69302e7046a946c5f4718ac5c324d1460e1d1db
mfjrp <mfjrp@proton.me>
2023-08-19 09:54:25 -0300

    Created project during the class and now push all to git

---
+++ src/domain/entity/order.spec.ts
@@ -0,0 +1,46 @@
+import Order from "./order";
+import OrderItem from "./order_item";
+
+describe("Order unit test", () => {
+
+    it("should throw error when id is empty", () => {
+        expect(() => {
+            let order = new Order("", "123", []);
+        }).toThrowError("Id is required");
+    });
+
+    it("should throw error when customerId is empty", () => {
+        expect(() => {
+            let order = new Order("1", "", []);
+        }).toThrowError("CustomerId is required");
+    });
+    
+    it("should throw error when customerId is empty", () => {
+        expect(() => {
+            let order = new Order("123", "123", []);
+        }).toThrowError("Item are required");
+    });
+
+    it("should calculate total", () => {
+        const item = new OrderItem("i1", "Item 1", 100, "p1", 2);
+        const item2 = new OrderItem("i2", "Item 2", 200, "p2", 2);
+        const order = new Order("o1", "c1", [item]);
+        
+        let total = order.total();
+
+        expect(total).toBe(200);
+
+        const order2 = new Order("o1", "c1", [item, item2]);
+        total = order2.total();
+
+        expect(total).toBe(600);
+    });
+
+    it("should throw error if the item quantity is less or equal than 0", () => {
+        expect(() => {
+            const item = new OrderItem("i1", "Item 1", 100, "p1", 0);
+            const order = new Order("o1", "c1", [item]);
+        }).toThrowError("Quantity must be grater than 0");;
+
+    });
+});
